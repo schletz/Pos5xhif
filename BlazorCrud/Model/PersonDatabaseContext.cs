bdcec79641a2ffd1a9cda5f945399b53a00c24c4
Michael Schletz <schletz@spengergasse.at>
2019-12-01 21:08:22 +0100

    Commit 20191201

---
+++ BlazorCrud/Model/PersonDatabaseContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BlazorCrud.Model
+{
+    public partial class PersonDatabaseContext : DbContext
+    {
+        public PersonDatabaseContext()
+        {
+        }
+
+        public PersonDatabaseContext(DbContextOptions<PersonDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("DataSource=PersonDatabase.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.P_ID).ValueGeneratedOnAdd();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
