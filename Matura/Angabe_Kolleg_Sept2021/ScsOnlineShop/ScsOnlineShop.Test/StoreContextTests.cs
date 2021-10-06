424cc0e64a0cc74e05cb6ddf7bc11d1fd207a27f
Michael Schletz <schletz@spengergasse.at>
2021-10-06 11:20:34 +0200

    Commit 20211006

--- Matura/Angabe_Kolleg_Sept2021/ScsOnlineShop/ScsOnlineShop.Test/StoreContextTests.cs
+++ Matura/Angabe_Kolleg_Sept2021/ScsOnlineShop/ScsOnlineShop.Test/StoreContextTests.cs
@@ -9,6 +9,7 @@ using Xunit;
 
 namespace ScsOnlineShop.Test
 {
+    [Collection("Sequential")]
     public class StoreContextTests
     {
         private StoreContext GetStoreContext()
@@ -46,7 +47,25 @@ namespace ScsOnlineShop.Test
             db.SaveChanges();
             // Act & Assert
             Assert.True(store.IsInBusinessTime(new DateTime(2021, 10, 4, 10, 0, 0)));
-
+        }
+        [Fact]
+        public void IsInBusinessTimeReturnsFalseTest()
+        {
+            using var db = GetStoreContext();
+            // Arrange
+            var store = new ActiveStore(new Store(
+                location: "Top 10",
+                floor: "1 OG",
+                companyName: "Zara",
+                state: StoreState.Active,
+                deliveryOption: DeliveryOption.Postal));
+            var businessHour = new BusinessHour(
+                new OpeningHour(
+                    dayOfWeek: DayOfWeek.Monday, hourFrom: 8, hourTo: 16, store: store));
+            db.BusinessHours.Add(businessHour);
+            db.SaveChanges();
+            // Act & Assert
+            Assert.False(store.IsInBusinessTime(new DateTime(2021, 10, 4, 16, 10, 0)));
         }
     }
 }
