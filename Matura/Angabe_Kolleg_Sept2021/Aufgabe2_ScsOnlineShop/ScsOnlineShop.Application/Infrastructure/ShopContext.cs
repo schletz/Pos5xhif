d262747c82bd1787dc93b9f14d3769196884be22
Michael Schletz <schletz@spengergasse.at>
2021-10-08 10:21:52 +0200

    Commit 20211008

---
+++ Matura/Angabe_Kolleg_Sept2021/Aufgabe2_ScsOnlineShop/ScsOnlineShop.Application/Infrastructure/ShopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScsOnlineShop.Application.Infrastructure
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions opt) : base(opt) { }
+    }
+}
